5331ce16db32e701e1c7f9ff6d3dc5fb012ce67f
syahrul-romadhon <81957394+syahrul-romadhon@users.noreply.github.com>
2023-06-09 17:58:51 +0700

    added more info to log

--- src/pendaftar.js
+++ src/pendaftar.js
@@ -3,126 +3,163 @@ import puppeteer from "puppeteer";
 import { creds } from "../config/cred-secret.js";
 
 function delay(time) {
-    return new Promise(function(resolve) { 
-        setTimeout(resolve, time)
-    });
+  return new Promise(function (resolve) {
+    setTimeout(resolve, time);
+  });
 }
 
-export async function scrapePendaftar ({
-    url,
-    sekolahID,
-    type
-}) {
+export async function scrapePendaftar({ url, sekolahID, type }) {
+  if (!url) {
+    throw new Error("Please provide URL as a first argument");
+  }
 
-    if (!url) {
-        throw new Error("Please provide URL as a first argument");
-    }
+  const BASE_URL = encodeURI(`${url}/${sekolahID}`);
+  const startTimer = new Date();
 
-    const BASE_URL = encodeURI(`${url}/${sekolahID}`)
-    const startTimer = new Date();
-    
-    const title = `${(startTimer.toJSON().slice(0, 10))} ${startTimer.getHours()} ${startTimer.getMinutes()} ${startTimer.getSeconds()}`;
+  const title = `${startTimer
+    .toJSON()
+    .slice(
+      0,
+      10
+    )} ${startTimer.getHours()} ${startTimer.getMinutes()} ${startTimer.getSeconds()}`;
 
-    let currPage = 1;
-    let pageCount = 0;
-    let dataCount = 0;
+  let currPage = 1;
+  let pageCount = 0;
+  let dataCount = 0;
 
-    const browser = await puppeteer.launch({
-        headless: "new", 
-        ignoreHTTPSErrors: true,
-        defaultViewport: null
+  const browser = await puppeteer.launch({
+    headless: "new",
+    ignoreHTTPSErrors: true,
+    defaultViewport: null,
+  });
+
+  const page = await browser.newPage();
+
+  try {
+    console.log("> Connecting to google spreadsheet..");
+    const doc = new GoogleSpreadsheet(creds.spreadsheet_id);
+    await doc.useServiceAccountAuth(creds);
+    await doc.loadInfo();
+    console.log("> Done!\n");
+
+    console.log("> Creating new sheet..");
+    const sheet = await doc.addSheet({
+      title: title,
+      headerValues: [
+        "No",
+        "Nomor Pendaftaran",
+        "Nama Pendaftar",
+        "Pilihan 1",
+        "Pilihan 2",
+        "Skor",
+      ],
     });
-    
-    const page = await browser.newPage();
+    console.log("> Done!\n");
 
-    try {
-        console.log('> Connecting to google spreadsheet..')
-        const doc = new GoogleSpreadsheet(creds.spreadsheet_id)
-        await doc.useServiceAccountAuth(creds)
-        await doc.loadInfo()
-        console.log('> Done!\n')
-        
-        console.log('> Creating new sheet..')
-        const sheet = await doc.addSheet({title: title, headerValues: [
-            'No',
-            'Nomor Pendaftaran',
-            'Nama Pendaftar',
-            'Pilihan 1',
-            'Pilihan 2',
-            'Skor',
-        ] })
-        console.log('> Done!\n')
+    console.log("> Opening url\n", BASE_URL);
 
-        console.log('> Opening url\n', BASE_URL)
-    
-        await page.goto(BASE_URL, {
-            waitUntil: ['domcontentloaded', 'networkidle0', 'networkidle2']
+    await page.goto(BASE_URL, {
+      waitUntil: ["domcontentloaded", "networkidle0", "networkidle2"],
+    });
+
+    // Set screen size
+    await page.setViewport({ width: 1920, height: 1300 });
+
+    // Select options
+    const node = await page.waitForSelector("select#type", {
+      timeout: 1000,
+      visible: true,
+    });
+    node.select(type);
+
+    await page.waitForResponse(async (response) => {
+      const httpResponse = await response.json();
+      pageCount = httpResponse.result.paginator.pageCount;
+
+      return (
+        response.url().includes(`/registrant?page=${currPage}`) &&
+        response.status() === 200
+      );
+    });
+
+    let result = [];
+
+    for (let i = currPage; i <= pageCount; i++) {
+      await delay(2000);
+      console.log("> Scrapping table data...\n");
+
+      const dataSource = await page.$$eval("tbody tr", (rows) => {
+        return Array.from(rows, (row) => {
+          const columns = row.querySelectorAll("td");
+          return Array.from(columns, (column) => column.innerText);
         });
-        
-        // Set screen size
-        await page.setViewport({width: 1920, height: 1300});
+      });
 
-        // Select options
-        const node = await page.waitForSelector('select#type', {timeout: 1000, visible: true});
-        node.select(type);
-            
-        await page.waitForResponse(async response => {
-            const httpResponse = await response.json();
-            pageCount = httpResponse.result.paginator.pageCount;
-    
-            return response.url().includes(`/registrant?page=${currPage}`) && response.status() === 200;
-        });
+      result = [...result, ...dataSource];
 
-        let result = [];
-    
-        for(let i = currPage; i <= pageCount; i++) {
-    
-            await delay(2000);
-            console.log('> Scrapping table data...\n');
+      dataCount += result.length;
 
-            const dataSource = await page.$$eval('tbody tr', rows => {
-                return Array.from(rows, row => {
-                  const columns = row.querySelectorAll('td');
-                  return Array.from(columns, column => column.innerText);
-                });
-            });
+      console.log(">Taking screenshot..", currPage);
 
-            result = [...result, ...dataSource]
+      await page.screenshot({
+        path: `screenshots/${title}-page-${currPage}.jpeg`,
+        fullPage: true,
+        type: "jpeg",
+        quality: 80,
+      });
+      console.log("> Done!\n");
 
-            dataCount += result.length;
-    
-            console.log('>Taking screenshot..', currPage);
-            
-            await page.screenshot({path: `screenshots/${title}-page-${currPage}.jpeg`, fullPage: true, type: 'jpeg', quality: 80});
-            console.log('> Done!\n')
-            
-            const element = await page.$('li.c-page-link:not(.disabled) > .c-page-link-next')
-    
-            if(element) {
-                currPage++;
-                console.log(`> Table pagination trigger, paginate to "${currPage}" data into spreadsheet...`)
-                await element.click();
-            }
-        }
+      const element = await page.$(
+        "li.c-page-link:not(.disabled) > .c-page-link-next"
+      );
 
-        console.log(`> Sorting result data...`)
-        result.sort((a, b) => parseFloat(b[5]) - parseFloat(a[5]))
-        console.log('> Done!\n')
-
-        console.log(`> Writing "${result.length}" data into spreadsheet...`)
-        await sheet.addRows(result);
-        console.log('> Done!\n')
-
-        const hasil = result.findIndex(a => a[2] === "ZHIZIAN SHABBYANNA MALTIM");
-        console.log(`> Hasil Seleksi(SEMENTARA) ${hasil + 1 <= 28 ? "Lulus" : "Tidak"} dengan nomor urut ${hasil + 1}`);
-        console.log('> Done!\n')
-
-    } catch(err) {
-        throw new Error(err);
-    } finally {
-        console.log(`END> Stopped in page: ${currPage}.`)
-        console.log(`END> Done writing ${dataCount} data into spreadsheet!`)
-        console.log(`END> Program exited with ${(new Date() - startTimer) / 1000} sec...`)
-        browser.close()
+      if (element) {
+        currPage++;
+        console.log(
+          `> Table pagination trigger, paginate to "${currPage}" data into spreadsheet...`
+        );
+        await element.click();
+      }
     }
+
+    console.log(`> Sorting result data...`);
+    result.sort((a, b) => parseFloat(b[5]) - parseFloat(a[5]));
+    console.log("> Done!\n");
+
+    console.log(`> Writing "${result.length}" data into spreadsheet...`);
+    await sheet.addRows(result);
+    console.log("> Done!\n");
+
+    console.log(
+      "====================== COLLECTING RESULT ===================================="
+    );
+    const hasil = result.findIndex((a) => a[2] === "ZHIZIAN SHABBYANNA MALTIM");
+    console.log(
+      `> Hasil Seleksi(SEMENTARA) ${
+        hasil + 1 <= 28 ? "Lulus" : "Tidak"
+      } dengan nomor urut ${hasil + 1}`
+    );
+    console.log("> Done!\n");
+  } catch (err) {
+    throw new Error(err);
+  } finally {
+    console.log(
+      "============================================================================="
+    );
+    console.log(`END> Stopped in page: ${currPage}.`);
+    console.log(`END> Done writing ${dataCount} data into spreadsheet!`);
+    console.log(
+      `END> Program exited with ${(new Date() - startTimer) / 1000} sec...`
+    );
+    console.log(
+      `Automatically generated at ${new Date()
+        .toJSON()
+        .slice(0, 19)} by: rmdhn.syahrul@gmail.com`
+    );
+    console.log(
+      "================================ END OF FILE ================================"
+    );
+    console.log(`\n\n\n\n\n`);
+    browser.close();
+  }
 }
